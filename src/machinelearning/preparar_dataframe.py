21f19d987d7b92afb7ad92107a90f9f499767ee3
Ambrosio Barceló Hurtado <ambrosio.barcelo.hurtado@gmail.com>
2025-03-07 09:56:11 +0100

    modificacion anio_matricula por antigüedad para modelos de predicción + 1er test con dos redes neuronales

--- src/machinelearning/preparar_dataframe.py
+++ src/machinelearning/preparar_dataframe.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import pickle
 import pathlib
+from datetime import datetime
 
 import sys
 import os
@@ -16,13 +17,17 @@ TARGET: str = "precio"
 df: pd.DataFrame = limpia.tratamiento_nans(f"{CURRENT_DIR}/../../data/coches_consolidado_limpio.csv")
 df = transforma_ML.normalizar(df, ["kilometraje", "potencia", "precio"])
 
-# Dejo solo las variables que nos interesan para el modelo
-df_modelo: pd.DataFrame = df[['kilometraje', 'cambio_automatico', 'potencia', 'marca_sola', 'anio_matricula', TARGET]] #cambiar anio_matricula por antiguedad
+anio_actual: int = datetime.now().year
+df['anio_matricula'] = df['anio_matricula'].apply(lambda x: anio_actual - x)
+
+# a raíz de la feature selection
+df_modelo: pd.DataFrame = df[['kilometraje', 'cambio_automatico', 'potencia', 'marca_sola', 'anio_matricula', TARGET]]
 
 with open(f"{CURRENT_DIR}/../../bin/cambio_automatico_encoder.pickle", "rb") as file:
     cambio_automatico_encoder: OneHotEncoder = pickle.load(file)
 
-df_modelo["cambio_automatico"] = cambio_automatico_encoder.transform(df_modelo[["cambio_automatico"]])
+cambio_automatico_transformed = cambio_automatico_encoder.transform(df_modelo[["cambio_automatico"]]).astype(bool)
+df_modelo.loc[:, "cambio_automatico"] = cambio_automatico_transformed
 
 with open(f"{CURRENT_DIR}/../../bin/dataframe_ml.pickle", "wb") as file:
     pickle.dump(df_modelo, file)
