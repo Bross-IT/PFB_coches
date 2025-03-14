e158d096a65567a5e1e7e6c0147d0447f4127635
Ambrosio Barceló Hurtado <ambrosio.barcelo.hurtado@gmail.com>
2025-03-14 12:34:08 +0100

    actualización de explicación de modelos en cotiza tu coche

--- src/cotiza_tu_coche.py
+++ src/cotiza_tu_coche.py
@@ -4,6 +4,7 @@ import pickle
 import numpy as np
 import re
 import datetime
+import plotly.express as px
 from tensorflow.keras.models import load_model, model_from_json
 from limpieza import transforma_ML
 from machinelearning.modelo_dl import graficar_historial
@@ -43,9 +44,19 @@ def show():
         """)
         df_resultados_ml = pd.read_pickle("bin/resultados_modelos_ml.pickle")
         st.dataframe(df_resultados_ml)
+
+        fig = px.bar(df_resultados_ml, x='Modelo', y=['MSE', 'MAE', 'RMSE', 'R2'], barmode='group',
+                title='Resultados de los Modelos de Machine Learning',
+                labels={'value': 'Métrica', 'variable': 'Tipo de Métrica'})
+        st.plotly_chart(fig)
         
-        st.markdown("""
-            Por tanto, hemos seleccionado **Random Forest**, el cual nos da una precisión de predicción del **90%** tras la búsqueda de los mejores hiperparámetros.
+        with open("bin/best_score_ml.pickle", "rb") as file:
+            best_score_rf = pickle.load(file)
+        
+        porcentaje_rf = round(best_score_rf * 100)
+        
+        st.markdown(f"""
+            Por tanto, hemos seleccionado **Random Forest**, el cual nos da una precisión de predicción del **{porcentaje_rf}%** tras la búsqueda de los mejores hiperparámetros.
             ### DL (DEEP LEARNING)
             Tras probar con distintas variantes de una red neuronal completamente conectada de regresión, con distintos hiperparámetros, distintas variantes de arquitectura y con funciones de pérdida MSE y MAE, hemos determinado que la óptima es la siguiente:
         """)
@@ -131,80 +142,26 @@ def show():
     elif option == "Usar los modelos de predicción":
         st.title("Predicción del precio de tu coche")
         
-        if "step" not in st.session_state:
-            st.session_state["step"] = 0
-        if "datos_usuario" not in st.session_state:
-            st.session_state["datos_usuario"] = {}
+        kilometraje = st.number_input("Kilometraje", min_value=0, max_value=375000, value=50000, step=1000)
+        cambio_automatico = st.radio("¿Cambio automático?", ["No", "Sí"])
+        potencia = st.number_input("Potencia (CV)", min_value=10, max_value=2500, value=150, step=10)
+        marcas_validas = ['ABARTH', 'AIWAYS', 'ALFA', 'AUDI', 'ASTON', 'BENTLEY', 'BMW', 'CADILLAC', 'CHEVROLET', 'CHRYSLER', 'CITROEN', 'CUPRA', 'DACIA',
+                          'DFSK', 'DODGE', 'DR', 'DS', 'EVO', 'FERRARI', 'FIAT', 'FORD', 'GMC', 'HONDA', 'HYUNDAI', 'INEOS', 'INFINITI', 'JAGUAR', 'JAECOO',
+                          'JEEP', 'KGM', 'KIA', 'LAMBORGHINI', 'LANCIA', 'LAND-ROVER', 'LEAPMOTOR', 'LEXUS', 'LINCOLN', 'LIVAN', 'LOTUS', 'LYNK', 'MASERATI',
+                          'MAZDA', 'McLAREN', 'MERCEDES-BENZ', 'MG', 'MINI', 'MITSUBISHI', 'MORGAN', 'NISSAN', 'OMODA', 'OPEL', 'PEUGEOT', 'PORSCHE', 'RAM',
+                          'RENAULT', 'SAAB', 'SANTANA', 'SEAT', 'SKODA', 'SMART', 'SUBARU', 'SUZUKI', 'TESLA', 'TOYOTA', 'VOLKSWAGEN', 'VOLVO', 'WIESMANN',
+                          'XEV', 'YES!']
+        marca = st.selectbox("Marca", marcas_validas)
+        anio_matricula = st.number_input("Año de matriculación", min_value=1900, max_value=datetime.datetime.now().year, value=2015, step=1)
         
-        step = st.session_state["step"]
-        datos_usuario = st.session_state["datos_usuario"]
-        
-        if datos_usuario and step > 0:
-            st.markdown("### Datos seleccionados hasta ahora:")
-            for key, value in datos_usuario.items():
-                if key == "anio_matricula":
-                    st.write(f"**Año de matriculación:** {value}")
-                elif key == "cambio_automatico":
-                    st.write(f"**Cambio automático:** {value}")
-                else:
-                    st.write(f"**{key.replace('_', ' ').capitalize()}:** {value}")
-        
-        error = False
-        
-        if step == 0:
-            kilometraje = st.number_input("Kilometraje", min_value=0, max_value=375000, value=50000, step=1000)
-            if kilometraje < 0 or kilometraje > 375000:
-                st.error("Por favor, introduce un valor de kilometraje entre 0 y 375000.")
-                error = True
-            else:
-                datos_usuario["kilometraje"] = kilometraje
-        elif step == 1:
-            cambio_automatico = st.radio("¿Cambio automático?", ["No", "Sí"])
-            datos_usuario["cambio_automatico"] = cambio_automatico
-        elif step == 2:
-            potencia = st.number_input("Potencia (CV)", min_value=10, max_value=2500, value=150, step=10)
-            if potencia < 10 or potencia > 2500:
-                st.error("Por favor, introduce un valor de potencia entre 10 y 2500.")
-                error = True
-            else:
-                datos_usuario["potencia"] = potencia
-        elif step == 3:
-            marcas_validas = ['ABARTH', 'AIWAYS', 'ALFA', 'AUDI', 'ASTON', 'BENTLEY', 'BMW', 'CADILLAC', 'CHEVROLET', 'CHRYSLER', 'CITROEN', 'CUPRA', 'DACIA',
-                              'DFSK', 'DODGE', 'DR', 'DS', 'EVO', 'FERRARI', 'FIAT', 'FORD', 'GMC', 'HONDA', 'HYUNDAI', 'INEOS', 'INFINITI', 'JAGUAR', 'JAECOO',
-                              'JEEP', 'KGM', 'KIA', 'LAMBORGHINI', 'LANCIA', 'LAND-ROVER', 'LEAPMOTOR', 'LEXUS', 'LINCOLN', 'LIVAN', 'LOTUS', 'LYNK', 'MASERATI',
-                              'MAZDA', 'McLAREN', 'MERCEDES-BENZ', 'MG', 'MINI', 'MITSUBISHI', 'MORGAN', 'NISSAN', 'OMODA', 'OPEL', 'PEUGEOT', 'PORSCHE', 'RAM',
-                              'RENAULT', 'SAAB', 'SANTANA', 'SEAT', 'SKODA', 'SMART', 'SUBARU', 'SUZUKI', 'TESLA', 'TOYOTA', 'VOLKSWAGEN', 'VOLVO', 'WIESMANN',
-                              'XEV', 'YES!']
-            marca = st.selectbox("Marca", marcas_validas)
-        elif step == 4:
-            anio_matricula = st.number_input("Año de matriculación", min_value=1900, max_value=datetime.datetime.now().year, value=2015, step=1)
-            if anio_matricula < 1900 or anio_matricula > datetime.datetime.now().year:
-                st.error("Por favor, introduce un año de matriculación válido.")
-                error = True
-            else:
-                datos_usuario["anio_matricula"] = anio_matricula
-        
-        if step < 5:
-            if st.button("Atrás") and step > 0:
-                st.session_state["step"] -= 1
-                st.session_state["datos_usuario"].pop(list(datos_usuario.keys())[-1], None)
-                st.rerun()
-            
-            if st.button("Siguiente") and not error:
-                if step == 3:
-                    datos_usuario["marca"] = marca
-                st.session_state["step"] += 1
-                st.rerun()
-        
-        if step == 5:
-            modelo_prediccion = st.radio("Modelo de predicción", ["Random forest", "Red neuronal"])
+        if st.button("Estimar precio"):
             anio_actual = datetime.datetime.now().year
             df = pd.DataFrame({
-                "kilometraje": [datos_usuario["kilometraje"]],
-                "cambio_automatico": [1 if datos_usuario["cambio_automatico"] == "Sí" else 0],
-                "potencia": [datos_usuario["potencia"]],
-                "marca_sola": [datos_usuario["marca"]],
-                "anio_matricula": [anio_actual - datos_usuario["anio_matricula"]]
+                "kilometraje": [kilometraje],
+                "cambio_automatico": [1 if cambio_automatico == "Sí" else 0],
+                "potencia": [potencia],
+                "marca_sola": [marca],
+                "anio_matricula": [anio_actual - anio_matricula]
             })
             df = transforma_ML.normalizar(df, ["kilometraje", "potencia"])
             with open("bin/marca_sola_precio_encoder.pickle", "rb") as file:
@@ -213,15 +170,21 @@ def show():
                 min_max_scaler = pickle.load(file)
             df["marca_sola"] = marca_sola_precio_encoder.transform(df[["marca_sola"]])
             df = min_max_scaler.transform(df)
-            if modelo_prediccion == "Random forest":
-                with open("bin/mejor_modelo.pickle", "rb") as file:
-                    modelo = pickle.load(file)
-                prediccion = modelo.predict(df)[0]
-            else:
-                modelo = load_model("bin/modelo_dl.keras")
-                prediccion = modelo.predict(df)[0][0]
-            precio_estimado = np.exp(prediccion)
-            st.success(f"El precio estimado de tu coche en el mercado actual es de {precio_estimado:,.0f} €.".replace(",", "."))
+            
+            # Predicción con Random Forest
+            with open("bin/mejor_modelo.pickle", "rb") as file:
+                modelo_ml = pickle.load(file)
+            prediccion_ml = modelo_ml.predict(df)[0]
+            precio_estimado_ml = np.exp(prediccion_ml)
+            
+            # Predicción con Red Neuronal
+            modelo_dl = load_model("bin/modelo_dl.keras")
+            prediccion_dl = modelo_dl.predict(df)[0][0]
+            precio_estimado_dl = np.exp(prediccion_dl)
+            
+            st.success(f"Estas son las estimaciones de precio de tu coche según modelo de predicción.\n"
+                       f"- ML (random forest): {precio_estimado_ml:,.0f} €.".replace(",", ".") + "\n"
+                       f"- DL (red neuronal): {precio_estimado_dl:,.0f} €.".replace(",", "."))
             if st.button("Reiniciar"):
                 st.session_state.clear()
                 st.rerun()
